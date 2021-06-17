9b567c8ff0cec9c2fdbfcca96c84ee1e9e66df4c
rawandrrr <>
2021-06-17 10:31:07 +0000

    lab5 style.css

---
+++ App.js
@@ -0,0 +1,30 @@
+var Name = "RAWAND";
+console.log(Name)
+console.log("hello world")
+
+
+
+var name = prompt("What is ur name?")
+
+alert("Welcome tp Action movies Website" + Name )
+
+var favmovie = prompt("what is ur favmovies from these (herry potter , forst gump)?")
+
+
+
+while(favmovie != "herry potter" && favmovie != "forst gump"){
+  
+  favmovie = prompt ("u enter wrong movie pleas try again")
+}
+console.log(favmovie)
+
+
+
+if(favmovie == "herry potter")
+
+{
+  document.write("<p>"+ "great thing u can see coz we have it"+"</p>")
+  }
+
+  else if(favmovie == "forst gump")
+  {document.write("<p>"+ "nice we happy for u coz we have ur favmovie"+"<p/>")}
