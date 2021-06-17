8704381b643a70f42e631d8c382ce23e42c4c28d
rawandrrr <>
2021-06-17 11:15:38 +0000

    lab4

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+
 var Name = "RAWAND";
 console.log(Name)
 console.log("hello world")
@@ -6,25 +7,19 @@ console.log("hello world")
 
 var name = prompt("What is ur name?")
 
-alert("Welcome tp Action movies Website" + Name )
+alert("Welcome tp Action movies Website " + name)
+
+
 
 var favmovie = prompt("what is ur favmovies from these (herry potter , forst gump)?")
 
-
-
-while(favmovie != "herry potter" && favmovie != "forst gump"){
-  
-  favmovie = prompt ("u enter wrong movie pleas try again")
+if (favmovie == "herry potter") {
+  document.write("<p>" + "great thing u can see coz we have it" + "</p>")
 }
-console.log(favmovie)
 
+else if (favmovie == "forst gump") { document.write("<p>" + "nice we happy for u coz we have ur favmovie" + "<p/>") }
 
+else (
+  document.write("<p>" + "you should pick one of them" + "<p/>")
+)
 
-if(favmovie == "herry potter")
-
-{
-  document.write("<p>"+ "great thing u can see coz we have it"+"</p>")
-  }
-
-  else if(favmovie == "forst gump")
-  {document.write("<p>"+ "nice we happy for u coz we have ur favmovie"+"<p/>")}
